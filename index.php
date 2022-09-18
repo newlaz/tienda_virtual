541e844756783ee7357658e327d6c5e7acd65524
sleepyzzack <109996822+sleepyzzack@users.noreply.github.com>
2022-09-18 03:34:00 -0600

    .htaccess actualizado e index refactoring
    le cambie unas weas al index y le agregue otras weas xd

--- index.php
+++ index.php
@@ -7,6 +7,21 @@ $params = "";
 
 if (!empty($arrUrl[1])) {
     if ($arrUrl[1] != "") {
-        $method = $arrUrl[0];
+        $method = $arrUrl[1];   
     }
 }
+
+if(!empty($arrUrl[1]) && $arrUrl[1] != ""){
+    for($i=2; $i< count($arrUrl); $i++){
+        $params .= $arrUrl[$i]."|";
+
+    }
+}
+echo "<br>";
+$params = trim($params, '|');
+echo "<br>";
+echo "<br>controlador: ".$controller. "<br>metodo: ". $method. "  <br><br>parametros:".$params;
+echo "<br>";
+print_r($arrUrl);
+
+?>
