537ecf6d148afb543fdc75d93e1642d34c60488c
sleepyzzack <109996822+sleepyzzack@users.noreply.github.com>
2022-09-19 05:28:40 -0600

    update 2
    contexto: https://youtu.be/KPQXl6QxFo8

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require_once('Config/config.php');
+
 $url = !empty($_GET['url']) ? $_GET['url'] : 'home/home';
 $arrUrl = explode('/', $url);
 $controller = $arrUrl[0];
@@ -17,11 +19,38 @@ if(!empty($arrUrl[1]) && $arrUrl[1] != ""){
 
     }
 }
-echo "<br>";
+
+spl_autoload_register(function($class){
+
+    if(file_exists(LIBS.'Core/'.$class.".php")){
+        require_once(LIBS.'Core/'.$class.".php");   
+    }    
+});
+
+//load xd
+
+$controller_file = "Controllers/".$controller.".php";
+
+if(file_exists($controller_file)){
+    require_once($controller_file);
+    $controller = new $controller();
+    if(method_exists($controller, $method)){
+        $controller->{$method}($params);
+
+    }else{
+        echo "no encontre ningun METODO, solo pude armar un taladro con una piedra, una ardilla, un palo y un taladro";
+    }
+
+}else{
+    echo "no encontre ningun CONTROLADOR, solo pude armar un taladro con una piedra, una ardilla, un palo y un taladro";
+}
+
+
+/*
 $params = trim($params, '|');
 echo "<br>";
 echo "<br>controlador: ".$controller. "<br>metodo: ". $method. "  <br><br>parametros:".$params;
 echo "<br>";
-print_r($arrUrl);
+*/
 
 ?>
