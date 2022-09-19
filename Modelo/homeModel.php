e8de935454e3e6b0889b1e4e5022d0b5ad71d3da
sleepyzzack <109996822+sleepyzzack@users.noreply.github.com>
2022-09-19 05:29:16 -0600

    Add files via upload

---
+++ Modelo/homeModel.php
@@ -0,0 +1,18 @@
+<?php 
+
+class homeModel{
+
+    public function __construct()
+    {
+        echo " ---  pinocho no sabia que era de madera, se hizo una paja y se prendio en candela";
+    }
+
+
+}
+
+
+
+
+
+
+?>
