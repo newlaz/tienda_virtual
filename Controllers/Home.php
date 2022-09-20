546594d3f48fb72380d8b8c460bccbc49053bda5
Bryan Lazo <lebry4n@gmail.com>
2022-09-19 20:08:55 -0600

    Creando clase home y verificando recepcion de parametros

---
+++ Controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+class Home
+{
+    public function __construct(){
+    }
+
+    public function home($params){
+        echo "Mensaje desde el controlador";
+    }
+
+    public function datos($params){
+        echo "Dato recibido: ".$params;
+    }
+}
