638bfeaa9055b73c3a27ffdb98ac9a9f37a678bf
Bryan Lazo <lebry4n@gmail.com>
2022-09-19 20:53:08 -0600

    Creacion de modelo home y creacion de metodo get

---
+++ Models/homeModel.php
@@ -0,0 +1,13 @@
+<?php 
+
+class homeModel {
+    public function __construct()
+    {
+        // echo 'Mensaje desde el modelo Home';
+    }
+
+    public function getCarrito($params)
+    {
+        return "Datos del carrito No. " . $params;
+    }
+}
