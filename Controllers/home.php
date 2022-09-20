6841e5438866a18312f62b4fe2dde285a049b875
Bryan Lazo <lebry4n@gmail.com>
2022-09-19 18:20:35 -0600

    Revirtiendo cambios no documentados

--- Controllers/home.php
+++ Controllers/home.php
@@ -1,20 +1,4 @@
-<?php 
-require_once('Libraries/Core/controllers.php');
 
-class home extends controllers{
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    public function home(){
-        echo " | cerote.com ";
-
-    }
-
-
-}
 
 
 
